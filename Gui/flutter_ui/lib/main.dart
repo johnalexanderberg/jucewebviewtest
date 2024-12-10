6fed9dd3d54af4404306033f87b6b1ced8a46450
alexanderberg <alexjohn.b@gmail.com>
2024-12-10 23:04:35 +0100

    use slider param widget

--- Gui/flutter_ui/lib/main.dart
+++ Gui/flutter_ui/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/slider_widget.dart';
-import 'dart:js' as js;
+import 'widgets/slider_param.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,89 +23,31 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 class _MyHomePageState extends State<MyHomePage> {
-  double _slidervalue = 0.5;
-  double _slidervalue2 = 0.0;
-
+  double _sliderValue = 0.5;
   @override
   void initState() {
     super.initState();
 
-    js.context.callMethod('setGainValueChangedFromDAWListener', [_onDAWGainValueChanged]);
   }
-
-  void _onSliderChanged(double value) {
-    setState(() {
-      _slidervalue = value;
-      try {
-        js.context.callMethod('sendToNative', [value]);
-      } catch (e, stack) {
-        print('Caught error: $e');
-        print('Stack trace: $stack');
-      }
-    });
-  }
-
-  void _onGainChangeStart(double value) {
-    js.context.callMethod('gainSliderDragStarted', []);
-  }
-
-  void _onGainChangeEnd(double value) {
-    js.context.callMethod('gainSliderDragEnded', []);
-  }
-
-  void _onGainSliderChanged(double value) {
-    setState(() {
-      _slidervalue2 = value;
-      js.context.callMethod('gainSetNormalisedValue', [value]);
-    });
-  }
-
-  void _onDAWGainValueChanged(double value) {
-    // This function will be called when the DAW updates the value
-    setState(() {
-      _slidervalue2 = value;
-    });
-  }
-
-  double _getGainSliderValue() {
-    return js.context.callMethod('gainGetNormalisedValue');
-  }
-
     @override
     Widget build(BuildContext context) {
-      return Scaffold(
+      return const Scaffold(
         body: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                "Slider 1 - native function",
+             Text(
+                "Attached to Juce gain parameter",
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
-              SliderWidget(
-                value: _slidervalue,
-                onChanged: _onSliderChanged,
-              ),
-              SizedBox(height: 20),
-
-              const Text(
-                "Slider 2 - attached to Juce gain parameter",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              SliderWidget(
-                  value: _slidervalue2,
-                  onChanged: _onGainSliderChanged,
-                  onChangeStart: _onGainChangeStart,
-                  onChangeEnd: _onGainChangeEnd
-              ),
+              SliderParam(parameterId: 'gain'),
             ],
           ),
         ),
